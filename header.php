b30bd58e3b2ad78165dab5ef7eec474576388447
Christian Oliff <christianoliff@yahoo.com>
2015-11-25 17:10:15 +0900

    Update header.php
    Updated HTML5Shiv and Bootstrap to latest versions

--- header.php
+++ header.php
@@ -6,11 +6,11 @@
  
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
 <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
   <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 <![endif]-->
 
