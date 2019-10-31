e35c428113c71d41ba4502145c3c515f804fdbf8
Abdul Wahab <wahabdigitall@yahoo.com>
2019-10-31 17:34:39 +0500

    Add required CSS classes and enqueue style properly.

--- footer.php
+++ footer.php
@@ -10,9 +10,6 @@
 
     </div><!-- /.container -->
 
-<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-
 <?php wp_footer(); ?>
 </body>
 </html>
